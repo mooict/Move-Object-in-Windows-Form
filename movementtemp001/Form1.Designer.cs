b36025d0ae57f30d4efe0e84434bd1c9de98d28a
mooict <67914277+mooict@users.noreply.github.com>
2021-01-03 12:50:37 +0000

    Add files via upload

---
+++ movementtemp001/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace movementtemp001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.player.Location = new System.Drawing.Point(670, 209);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 72);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.playertime);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 528);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Object Movements MooICT";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ movementtemp001/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace movementtemp001
+{
+    public partial class Form1 : Form
+    {
+
+        int playerX = 15;
+        int playerY = 15;
+
+        bool moveLeft, moveRight, moveUp, moveDown;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playertime(object sender, EventArgs e)
+        {
+
+            if(moveLeft && player.Left > 0)
+            {
+                player.Left -= playerX;
+            }
+            if(moveRight && player.Left + player.Width < 781)
+            {
+                player.Left += playerX;
+            }
+
+            if(moveUp && player.Top > 0)
+            {
+                player.Top -= playerY;
+            }
+
+            if(moveDown && player.Top + player.Height < 567)
+            {
+                player.Top += playerY;
+            }
+
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = false;
+            }
+        }
+    }
+}
